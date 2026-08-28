0ba4ff99a8eeac58ca049163b49b4ed669eb944f
tanuhaveri <tanushreehaveri17@gmail.com>
2026-08-28 20:54:16 +0530

    feat: add hobby search and category filters

--- script.js
+++ script.js
@@ -17,3 +17,66 @@ function closeHobby() {
 
     modal.style.display = "none";
 }
+
+const searchInput = document.getElementById("hobby-search");
+const hobbyCards = document.querySelectorAll(".hobby-card");
+const filterButtons = document.querySelectorAll(".filter-button");
+
+let selectedCategory = "all";
+
+
+function filterHobbies() {
+
+    const searchText = searchInput.value.toLowerCase();
+    
+     let visibleCards = 0;
+
+    hobbyCards.forEach(function (card) {
+
+
+        const hobbyName = card.querySelector("h3").textContent.toLowerCase();
+        const hobbyCategory = card.dataset.category;
+
+        const matchesSearch = hobbyName.includes(searchText);
+        const matchesCategory =
+            selectedCategory === "all" ||
+            hobbyCategory === selectedCategory;
+
+        if (matchesSearch && matchesCategory) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    
+    });
+    const noResults = document.getElementById("no-results");
+
+    if (visibleCards === 0) {
+    noResults.style.display = "block";
+    } else {
+    noResults.style.display = "none";
+    }
+
+}
+
+
+searchInput.addEventListener("input", filterHobbies);
+
+
+filterButtons.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+        selectedCategory = button.dataset.category;
+
+        filterButtons.forEach(function (btn) {
+            btn.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+        filterHobbies();
+
+    });
+
+});
